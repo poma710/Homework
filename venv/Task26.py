566de4b639299dc1f8f2e7d82c2e0901032d548e
unknown <poma710@yandex.ru>
2023-07-03 13:49:12 +0300

    add Task26

--- venv/Task26.py
+++ venv/Task26.py
@@ -5,5 +5,17 @@
 A = 3; B = 5 -> 243 (3⁵)
     A = 2; B = 3 -> 8  """
 
-A = int(input("Введите первое число: "))
-B = int(input("Введите второе число: "))
+def power(a, b):
+ if b == 0:
+  return 1
+ elif b > 0:
+  return a * power(a, b - 1)
+ else:
+  return 1 / power(a, -b)
+
+A = int(input("Введите число: "))
+B = int(input("Введите степень: "))
+result = power(A, B)
+print(result)
+
+
